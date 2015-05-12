9a51ee5b6ccc721e223dd966a50b909510f038f5
sanmaowang <wang@sanmao.me>
2015-05-13 00:23:50 +0800

    less preprocessor

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var connect = require('connect');
 var makeJade = require('./lib/processor/jade');
+var makeLess = require('./lib/processor/less');
 
 function createMiniHarp(root){
   var app = connect();
   app.use(makeJade(root));
+  app.use(makeLess(root));
   return app;
 };
 
