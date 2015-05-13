7a86a4bda86cb8a4a2e52231c2ac8d14563856ac
sanmaowang <wang@sanmao.me>
2015-05-13 11:28:52 +0800

    lesson 7

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var serveStatic = require('serve-static');
 var makeJade = require('./lib/processor/jade');
 var makeLess = require('./lib/processor/less');
 var makeRewrite = require('./lib/processor/rewrite');
+var makeBlock = require('./lib/processor/block');
 
 
 function createMiniHarp(root){
   var app = connect();
-	app.use(serveStatic(root));
+	// app.use(serveStatic(root));
   app.use(makeRewrite(root));
+  app.use(makeBlock(root));
   app.use(makeLess(root));
   app.use(makeJade(root));
 
