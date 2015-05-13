ec4bea478df868659ea7c3877e3adbb18cdb7aac
sanmaowang <wang@sanmao.me>
2015-05-13 11:06:41 +0800

    make rewrite

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,25 +1,23 @@
 #! /usr/bin/env node
 var createMiniHarp = require("mini-harp");
 var parseArgs = require('minimist');
-var serveStatic = require('serve-static');
 
 var argv = parseArgs(process.argv.slice(2));
 
 var port = argv.port || 4000;
 
-var app = createMiniHarp();
 
-var path = argv._[0] || process.cwd();
+var root = argv._[0] || process.cwd();
 
-app.use('/current-time',function(req,res,next){
-    res.end(new Date().toISOString());
-});
+var app = createMiniHarp(root);
 
 
-app.use(serveStatic(path));
+
+
 
 
 app.listen(port);
 
 console.log("Starting mini-harp on http://localhost:"+port);
+console.log(root);
 
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var connect = require('connect');
+var serveStatic = require('serve-static');
 var makeJade = require('./lib/processor/jade');
 var makeLess = require('./lib/processor/less');
+var makeRewrite = require('./lib/processor/rewrite');
+
 
 function createMiniHarp(root){
   var app = connect();
-  app.use(makeJade(root));
+	app.use(serveStatic(root));
+  app.use(makeRewrite(root));
   app.use(makeLess(root));
+  app.use(makeJade(root));
+
+// app.use('/current-time',function(req,res,next){
+//     res.end(new Date().toISOString());
+// });
   return app;
 };
 
